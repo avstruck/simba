297a6a0b8560f860665ae073d41f7a42d00a0d71
ChristianLiu <christianliu@college.harvard.edu>
2014-06-12 23:27:29 +0200

    fixed css errors and made blank footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,20 +4,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.	
   protect_from_forgery with: :exception
+  
   private
+  
   def configure_permitted_parameters
- 	devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :username, :password, :password_confirmation, :bio, :address) }
+ 	  devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :username, :password, :password_confirmation, :bio, :address) }
   end
 end
-
-<!--   <% if current_user %>
-    <p>Are you an artist? Upload your art <%= link_to "here", "/pieces/new" %>.</p>
-  <% end %>
-
-  <% if current_user %>
-    <p>You're signed in as <%= current_user.username %>.</p>
-    <p><%= link_to "Profile", "/users/#{current_user.id}" %></p>
-    <p><%= link_to "Logout", destroy_user_session_path, method: :delete %></p>
-  <% else %>
-    
-  <% end %>-->
