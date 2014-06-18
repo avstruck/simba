a159e7d0efbc0552dfa3ab566f94f6b70e6d0902
ChristianLiu <christianliu@college.harvard.edu>
2014-06-18 21:45:17 +0200

    removed failed login stuff because it failed

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-config.autoload_paths  = %W(#{config.root}/lib)
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
